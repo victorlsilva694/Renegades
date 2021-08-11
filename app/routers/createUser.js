57acf53a23801c70dc260f0352954ea6911620a9
José Júnior <72272209+josesfjunior@users.noreply.github.com>
2021-08-10 21:34:46 -0300

    Criação do metodo Delete

--- app/routers/createUser.js
+++ app/routers/createUser.js
@@ -3,16 +3,31 @@ const router = express.Router();
 const bd = require("../Schema/connection");
 
 router.get("/", async (req, res) => {
-  const listUser = await bd.query("SELECT * FROM Testes");
-  res.send(listUser.rows);
+  try {
+    const listUser = await bd.query("SELECT * FROM Users");
+    res.send(listUser.rows).status(200);
+  } catch (err) {
+    res.send(err).status(400);
+  }
 });
 
 router.post("/", async (req, res) => {
-  const post = await bd.query(
-    `INSERT INTO Users(name,lastName,CPF,RG) VALUES ('${req.body.name}','${req.body.lastName}','${req.body.cpf}','${req.body.rg}')`
-  );
-
-  res.send("ok");
+  try {
+    await bd.query(
+      `INSERT INTO Users(name,lastName,CPF,RG) VALUES ('${req.body.name}','${req.body.lastName}','${req.body.cpf}','${req.body.rg}')`
+    );
+    res.status(201).send("Ok");
+  } catch (err) {
+    res.send(err).status(400);
+  }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    await bd.query(`DELETE FROM Users WHERE id = ${req.params.id}`)
+    res.status(200).send("Deletado")
+  }catch (err) {
+    res.status(204).send("Não foi deletado")
+  }
+})
 module.exports = router;
